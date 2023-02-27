e21fe06726d2407a3d8f12d47035129f7fecbc5f
Dario Berberi <116946217+dario-berberi@users.noreply.github.com>
2023-02-27 14:28:45 +0100

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife
+      </Link>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
